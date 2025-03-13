7c397167897586f37cb2925836895e4fe15a3706
Netext <daniel.dimun@outlook.cz>
2025-03-13 22:12:59 +0100

    changed complete to PostGress and code remake functional

--- program.py
+++ program.py
@@ -1,63 +1,77 @@
 import os
 import sqlite3
-import mysql.connector
+import psycopg2  
 import json
 
 header = """
-             .__      __        ___.   .__                                        __                
-  ___________|  |   _/  |______ \_ |__ |  |   ____     ___________   ____ _____ _/  |_  ___________ 
- /  ___/ ____/  |   \   __\__  \ | __ \|  | _/ __ \  _/ ___\_  __ \_/ __ \\__  \\   __\/  _ \_  __ \
+             .__      **        **_.   .__                                        __                
+  ___________|  |   */  |*_____ \_ |__ |  |   ____     ___________   ____ _____ */  |*  ___________
+ /  ___/ ____/  |   \   **\**  \ | __ \|  | */ *_ \  */ *__\_  __ \_/ ** \\**  \\   **\/  ***** \*****  ** \
  \___ < <_|  |  |__  |  |  / __ \| \_\ \  |_\  ___/  \  \___|  | \/\  ___/ / __ \|  | (  <_> )  | \/
-/____  >__   |____/  |__| (____  /___  /____/\___  >  \___  >__|    \___  >____  /__|  \____/|__|   
-     \/   |__|                 \/    \/          \/       \/            \/     \/                   
+/____  >__   |____/  |__| (____  /___  /____/\___  >  \___  >__|    \___  >____  /__|  \____/|__|  
+     \/   |__|                 \/    \/          \/       \/            \/     \/                  
 """
 
-# main menu 
+# main menu
 print(header)
 print("=> Pro Vytvoření nové tabulky napiš create ")
 print("=> Pro exit napiš exit")
-
 interakce = input("Zadejte interakci: ")
 
 if interakce == "create":
-    # SQLite připojení a vytvoření tabulky
+   
     conn_sqlite = sqlite3.connect("database.db")
     cursor_sqlite = conn_sqlite.cursor()
-
     with open("table.sql", "r") as sql_file:
         cursor_sqlite.executescript(sql_file.read())
-
     conn_sqlite.commit()
     conn_sqlite.close()
-
     print("SQLite tabulka byla úspěšně vytvořena!")
-
-    
+   
     try:
-        conn_mysql = mysql.connector.connect(
-            host="localhost:2003",
-            user="root",
+        
+        conn_postgres = psycopg2.connect(
+            host="localhost",
+            user="postgres",  
+            password="123",   
+            port=5432         
+        )
+        conn_postgres.autocommit = True  
+        cursor_postgres = conn_postgres.cursor()
+        
+        
+        cursor_postgres.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'moje_databaze'")
+        exists = cursor_postgres.fetchone()
+        
+        if not exists:
+            cursor_postgres.execute("CREATE DATABASE moje_databaze")
+        
+        
+        conn_postgres.close()
+        
+        
+        conn_postgres = psycopg2.connect(
+            host="localhost",
+            user="postgres",
             password="123",
+            port=5432,
             database="moje_databaze"
         )
-        cursor_mysql = conn_mysql.cursor()
-
-        cursor_mysql.execute("""
+        cursor_postgres = conn_postgres.cursor()
+        
+        
+        cursor_postgres.execute("""
         CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
+            id SERIAL PRIMARY KEY,
             name VARCHAR(100) NOT NULL,
             age INT NOT NULL
         );
         """)
-
-        conn_mysql.commit()
-        conn_mysql.close()
-
-        print("MySQL tabulka byla úspěšně vytvořena!")
-
-    except mysql.connector.Error as err:
-        print(f"Chyba při připojení k MySQL: {err}")
-
+        conn_postgres.commit()
+        conn_postgres.close()
+        print("PostgreSQL tabulka byla úspěšně vytvořena!")
+    except Exception as err:
+        print(f"Chyba při připojení k PostgreSQL: {err}")
 else:
     print("Ukončuji program.")
     exit()
